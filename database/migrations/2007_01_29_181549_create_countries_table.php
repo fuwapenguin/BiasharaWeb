b61dfe34d660aa858656e5ac8fdd228215eb495b
Mitchel Emerenciana <mitchelmax96@hotmail.com>
2020-03-17 13:14:35 +0100

    Registration working

--- database/migrations/2007_01_29_181549_create_countries_table.php
+++ database/migrations/2007_01_29_181549_create_countries_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
+use Webpatser\Uuid\Uuid;
 
 class CreateCountriesTable extends Migration
 {
--- database/migrations/2008_02_11_130528_create_images_table.php
+++ database/migrations/2008_02_11_130528_create_images_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Webpatser\Uuid\Uuid;
 
 class CreateImagesTable extends Migration
 {
@@ -14,7 +15,7 @@ class CreateImagesTable extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-            $table->uuid('image_id');
+            $table->uuid('image_id')->default(Uuid::generate());
             $table->primary('image_id');
             $table->string('image_name');
             $table->string('image_code');
